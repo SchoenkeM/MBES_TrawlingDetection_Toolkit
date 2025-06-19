61c0c33db50723949e5c290797be8b7693113a29
SchoenkeM <mischa.schoenke@gmx.de>
2025-06-19 12:56:12 +0200

    Update execute and Main function in order to make the input more intuitive

--- Main_TM-Processing.py
+++ Main_TM-Processing.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Apr  3 11:41:27 2024
-version 0.1.1
+version 1.0.3
 @author: mischa reineccius-schonke
 """
 
@@ -21,31 +21,18 @@ Config_List = 'EMB267_config_demo.yaml'
 #Config_List = ['EMB238_config_demo.yaml', 'EMB267_config_demo.yaml', 'EMB288_config_demo.yaml', 'EMB320_config_demo.yaml', 'EMB345_config_demo.yaml']  
 
 
-'''    
-Optional "execute_task" function input arguments:
+Opperation_Procedure = {
+    'import_data': 'auto',           # use 'clean' to re-import all files in the folder/ #auto' only import *txt files that have not yet been converted 
+    'process_data': 'auto',          # use 'clean' to re-import all files in the folder/ #auto' only import *txt files that have not yet been processed
+    'merge_dataset': 'enabled',     # use 'enable'/disable to toggle on/Off
+    'grid_dataset': 'enabled',      # use 'enable'/disable to toggle on/Off
+    'export_tif': 'enabled',        # use 'enable'/disable to toggle on/Off
+    'export_gpkg': 'enabled'        # use 'enable'/disable to toggle on/Off
+    }
 
-,update_import= True       # will only import *txt files that have not yet been converted to *.df-files.
-                           
-,update_processing= True   # wills only process *.df-files that have not yet been porcessed yet
-             
-,make_tiles= False         # If tiles have already been created, the data can be re-gridded without need to created the tiles again
-                           
-,update_tif= True          # If processed data exists, only runs exports geotiffs from processed data. Ignore file import, processing and gridding
-                           
-,update_gpkg= True         # If processed data exists, only exports geopackage from processed data. Ignore file import, processing and gridding
-          
-
-# Examples
-# If new txt files have been added or only the grid option has been changed, use the following line:
-# execute_task(Config_List, update_import = True, update_processing = True)
-
-# If a new threshold is defined, use the following line to export the geopackage with the new threshold:
-# execute_task(Config_List, update_gpkg= True)
-
-'''
 
 # starts processing
-execute_task(Config_List, update_import = True)
+execute_task(Config_List, Opperation_Procedure)
 
 
 
--- execute_task_fct.py
+++ execute_task_fct.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Mar 20 15:48:18 2025
-
+version 1.0.3
 @author: misch
 """
 from config_handler import ConfigHandler
@@ -10,16 +10,62 @@ from data_handler import DataHandler
 from processing_handler import ProcessingHandler
 from output_handler import OutputHandler
 
-def execute_task(config_files, update_import = False, update_processing = False, update_tif= False, update_gpkg= False, make_tiles = True):
-
+#def execute_task(config_files, clean_import = False, clean_processing = False, export_tif= False, export_gpkg= False, compile_dataset = False, create_grid = False):
+def execute_task(config_files, opperation_procedure):
     # in case export update is defined, skipp processing stepts and only 
     # update export
-    skip_processing =  update_tif or update_gpkg
     
-    # Do not edit here
-    if isinstance(config_files, str):
+    def validate_pperation_procedure(opperation_procedure):
         
+        # Define allowed values per key group
+        allowed_values = {
+            "filter": {"enabled", "disabled"},
+            "mode": {"auto", "clean"}
+        }
+        print(">> Check Opperation Procedure Input Arguments...", end='')  # Log progress
+        input_para = {
+            "mode_1": opperation_procedure.get('import_data'),
+            "mode_2": opperation_procedure.get('process_data'),       
+            "filter_1": opperation_procedure.get('merge_dataset'),   
+            "filter_2": opperation_procedure.get('grid_dataset'),   
+            "filter_3":  opperation_procedure.get('export_tif'),       
+            "filter_4": opperation_procedure.get('export_gpkg')     
+            }
+
+        # Validation function
+        all_valid = True
+        for key, value in input_para.items():
+            matched = False
+            for prefix, valid_set in allowed_values.items():
+                if key.startswith(prefix):
+                    if value not in valid_set:
+                        index = list(input_para.keys()).index(key)
+                        option = list(opperation_procedure.keys())[index]
+                        print(f"\n\t- FAILED: Invalid Input Argument '{value}' for '{option}' [allowed: {valid_set}]")
+                        
+                        all_valid = False
+                    matched = True
+                    break
+            if not matched:
+                print(f"\nUnexpected config key: '{key}'")
+                all_valid = False
+        
+        if all_valid == True:
+            print(" [success]")
+        else:
+            print("\n>> One or more input options do not match the permitted specifications. Please check the opperation settings and restart the processing. ")
+            
+        return all_valid
+
+    # Do not edit here
+    if isinstance(config_files, str):       
         config_files = [config_files]   
+
+
+    is_valid_opperations = validate_pperation_procedure(opperation_procedure)
+    if not is_valid_opperations:
+        return
+    
 
     for config_file in config_files:
         
@@ -33,38 +79,44 @@ def execute_task(config_files, update_import = False, update_processing = False,
         # Initialize configuration and handlers
         config = ConfigHandler(config_file)  
         
-        # if no export update is set, init processing classes
-        if skip_processing:
-            Output_handler = OutputHandler(log, config)
-        else: 
-            MBES_data = DataHandler(log, config)
-            Processing_handler = ProcessingHandler(log, config)
-            Output_handler = OutputHandler(log, config)
+        Processing_handler = ProcessingHandler(log, config)
+        MBES_data = DataHandler(log, config)
+        Output_handler = OutputHandler(log, config)
         
+        # if no export update is set, init processing classes
+        """
+        if [opperation_procedure.get('merge_dataset') == 'enabled' or
+            opperation_procedure.get('grid_dataset') == 'enabled' or
+            opperation_procedure.get('export_tif') == 'enabled' or 
+            opperation_procedure.get('export_gpkg') == 'enabled']== True:
+        
+            Output_handler = OutputHandler(log, config)
+        """
         #______________________________________________________________________
         # Runs Processing Steps
         
-        if skip_processing:
-            # If update_tif is true
-            if update_tif:
-                Output_handler.export_geotiff()  # Export to GeoTIFF
+        if opperation_procedure.get('import_data') == 'auto':
+            MBES_data.import_data(True)  # if update import is true skipp already converted txt files
+        else:
+            MBES_data.import_data(False)  # if update import is true skipp already converted txt files
+
+        if opperation_procedure.get('process_data') == 'auto':
+            Processing_handler.process_data(True)  # Process dataset
+        else:
+            Processing_handler.process_data(False)  # Process dataset
             
-            # If update_gpkg is true
-            if update_gpkg:
-                Output_handler.export_statisics()  # Export statistics
-                
-        else:       
-            # converts txt2df
-            MBES_data.import_data(update_import)  # if update import is true skipp already converted txt files
-            # applies filter
-            Processing_handler.process_data(update_processing)  # Process dataset
+        if opperation_procedure.get('merge_dataset') == 'enabled':
+            Output_handler.compile_tiles()  # Compile tiles
             
-            # Output Handler
-            if make_tiles:
-                Output_handler.compile_tiles()  # Compile tiles
+        if opperation_procedure.get('grid_dataset') == 'enabled':   
             Output_handler.grid_tiles()  # Grid tiles
+        
+        if opperation_procedure.get('export_tif') == 'enabled':
             Output_handler.export_geotiff()  # Export to GeoTIFF
-            Output_handler.export_statisics()  # Export statistics
+            
+        if  opperation_procedure.get('export_gpkg') == 'enabled':
+            Output_handler.export_statisics()
+
 
         #Save configuration and log
         config.save_config()
